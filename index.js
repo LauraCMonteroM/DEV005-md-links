75495dfdd12049daf8ded4efe7eac518a09f0e6f
LauraCMonteroM <lauramontero205@gmail.com>
2023-05-23 11:19:57 -0500

    agrega dependecias

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) =>{
+    // ¿la ruta es válida?
+    if (fs.existsSync(path)){
+      // ¿es una ruta absoluta?
+      
+    } else {
+      // si no existe la ruta
+      reject ('La ruta no existe');
+    }
+  })
+  // Identificar si la ruta existe
+  
 };
