fac6b8316c15f14578af874cb2ce8cc28c4b4139
LauraCMonteroM <lauramontero205@gmail.com>
2023-06-05 12:27:34 -0500

    Changes

--- linksExtractor.js
+++ linksExtractor.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const extractLinks = (userPath) => new Promise((resolve, reject) => {
+const { paths } = require('./paths'); // borrar en un futuro
+
+const extractLinks = (userPath) => {
   fs.readFile(userPath, 'utf-8', (err, data) => {
     if (err) {
-      reject(err);
-      return;
+      return undefined;
     }
     const regExpLinks = /\[(.*?)\]\((.*?)\)/g;
     const links = [];
@@ -13,30 +14,19 @@ const extractLinks = (userPath) => new Promise((resolve, reject) => {
     let coincidence;
     while ((coincidence = regExpLinks.exec(data)) !== null) {
       const textLink = coincidence[1];
-      const urlLink = coincidence[2];
+      const url = coincidence[2];
 
-      const infolink = {
-        index: links.length + 1,
-        resolvedPath: path.resolve(userPath),
-        texto: textLink,
-        url: urlLink,
-      };
-      links.push(infolink);
-      console.log(links);
+      links.push({ texto: textLink, url });
     }
-
-    resolve(links);
+    const completeLinks = links.map((link) => ({
+      ...link,
+      file: path.resolve(userPath),
+    }));
+    console.log(completeLinks);
   });
-});
+};
 
-const userPath = './src/Prueba1'; // ruta de ejemplo
-extractLinks(userPath)
-  .then((links) => {
-    console.log(links); // Hacer algo con los links encontrados
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+extractLinks(paths('README.md')[1]);
 
 module.exports = {
   extractLinks,
